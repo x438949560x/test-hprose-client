2c20fd8939fb5bb67297ebd9fda056eacfca5bde
wuhui <438949560@qq.com>
2020-03-09 15:30:19 +0800

    Initial commit

---
+++ src/main/java/com/ccsu/testhproseclient/TestHproseClientApplication.java
@@ -0,0 +1,13 @@
+package com.ccsu.testhproseclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHproseClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHproseClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ccsu/testhproseclient/TestHproseClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccsu.testhproseclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHproseClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
